c7f16a76fa4d2c7ab983a71e1c1fa65e2494533f
Gustavo Covas <gtcovas@gmail.com>
2017-07-09 16:20:25 -0300

    Add hostfile to mpi command

--- src/run_measurements.sh
+++ src/run_measurements.sh
@@ -11,10 +11,10 @@ make
 mkdir results
 mkdir results/mpi
 
-perf stat -r $MEASUREMENTS mpirun -np $1 ./mandelbrot_mpi -2.5    1.5   -2.0    2.0   $SIZE >> full.log          2>&1
-perf stat -r $MEASUREMENTS mpirun -np $1 ./mandelbrot_mpi -0.8   -0.7    0.05   0.15  $SIZE >> seahorse.log      2>&1
-perf stat -r $MEASUREMENTS mpirun -np $1 ./mandelbrot_mpi  0.175  0.375 -0.1    0.1   $SIZE >> elephant.log      2>&1
-perf stat -r $MEASUREMENTS mpirun -np $1 ./mandelbrot_mpi -0.188 -0.012  0.554  0.754 $SIZE >> triple_spiral.log 2>&1
+perf stat -r $MEASUREMENTS mpirun -hostfile mpi_hosts -np $1 ./mandelbrot_mpi -2.5    1.5   -2.0    2.0   $SIZE >> full.log          2>&1
+perf stat -r $MEASUREMENTS mpirun -hostfile mpi_hosts -np $1 ./mandelbrot_mpi -0.8   -0.7    0.05   0.15  $SIZE >> seahorse.log      2>&1
+perf stat -r $MEASUREMENTS mpirun -hostfile mpi_hosts -np $1 ./mandelbrot_mpi  0.175  0.375 -0.1    0.1   $SIZE >> elephant.log      2>&1
+perf stat -r $MEASUREMENTS mpirun -hostfile mpi_hosts -np $1 ./mandelbrot_mpi -0.188 -0.012  0.554  0.754 $SIZE >> triple_spiral.log 2>&1
 
 mv *.log results/mpi
 rm output.ppm
